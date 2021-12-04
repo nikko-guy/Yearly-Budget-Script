2ff62c7a9dab389999fc863d723e965ce7125b8d
DrDaNkk <39970384+Dr-DaNk@users.noreply.github.com>
2021-12-04 12:50:56 -0600

    2

--- src/main.js
+++ src/main.js
@@ -1,16 +1,3 @@
-var commonNames = {};
-var transactions = {};
-var headerRows = {};
-var flags = {};
-var originalNames = {};
-
-const spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
-const spreadsheetID = spreadsheet.getId();
-const properties = PropertiesService.getDocumentProperties().getProperties();
-
-var normalText = SpreadsheetApp.newTextStyle().setBold(false).setFontFamily("Lato").setFontSize(11).setForegroundColor("#576475")
-  .setItalic(false).setStrikethrough(false).setUnderline(false).setBold(false).build();
-
 function run() {
   //reloadPrivacyTransactions();
   log("hello");
